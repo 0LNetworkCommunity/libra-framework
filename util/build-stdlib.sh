f7d1d6baf05e16dbbb2658e2ee7091cc9a538b0c
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-05-31 15:13:31 -0400

    clean

--- util/build-stdlib.sh
+++ util/build-stdlib.sh
@@ -3,8 +3,7 @@
 
 echo -e "\n0L: Building move framework ..."
 
-
 # release without source code
 cargo r -p libra-framework -- release -w
 
-printf "0L: Success building .mrb release bundle. Saved to: $OUT \n"
+printf "0L: Success building .mrb release bundle. Saved to: framework/releases/head.mrb \n"
