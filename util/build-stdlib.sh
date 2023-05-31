f488f4e193ee5dd26ddc9942be5316ed9e7a23de
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-05-31 11:46:36 -0400

    patch shell scripts

--- util/build-stdlib.sh
+++ util/build-stdlib.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-# set -e # exit on error
+#set -e # exit on error
 
 echo -e "\n0L: Building move framework ..."
-
-export OUT=(./framework/releases/framework_fresh.mrb)
-aptos-framework custom --packages ./framework --rust-bindings "" --output $OUT 
+unset OUT
+export OUT=$(cd ./framework/releases/ && pwd -P | xargs -I {} echo "{}/fresh.mrb")
+# echo $OUT
+$ZAPATOS/target/release/aptos-framework custom \
+  --packages $ZAPATOS/aptos-move/framework/move-stdlib \
+  --packages $ZAPATOS/aptos-move/framework/aptos-stdlib \
+  --packages ./framework \
+  --rust-bindings "" \
+  --rust-bindings "" \
+  --rust-bindings "" \
+  --output $OUT
 
 printf "0L: Success building .mrb release bundle. Saved to: $OUT \n"
--- util/smoke.sh
+++ util/smoke.sh
@@ -2,11 +2,12 @@
 # set -e # exit on error
 
 echo -e "\n0L: running smoke tests"
-
-export MRB_PATH=$(pwd | xargs -I % echo '%/framework/releases/framework_fresh.mrb')
+unset MRB_PATH
+export MRB_PATH=$(cd ./framework/releases/ && pwd -P | xargs -I {} echo "{}/fresh.mrb")
+export ZAPATOS_BIN_PATH=$ZAPATOS/target/release
 # export ZAPATOS_BIN_PATH=(~/code/rust/zapatos/target/release)
 # echo $ZAPATOS_BIN_PATH
 echo $MRB_PATH
 
-(cd smoke-tests && MRB_PATH=$MRB_PATH ZAPATOS_BIN_PATH=$(echo ~/code/rust/zapatos/target/release) cargo test -- --nocapture)
+(cd smoke-tests && MRB_PATH=$MRB_PATH ZAPATOS_BIN_PATH=$ZAPATOS/target/release cargo test -- --nocapture)
 
