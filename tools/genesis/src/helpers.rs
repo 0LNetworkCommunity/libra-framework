e0f48b18c8c1b129ecff0f4cba0d40783943018a
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-06-01 16:21:06 -0400

    [Genesis] fix validator network host publication (#9)

---
+++ tools/genesis/src/helpers.rs
@@ -0,0 +1,15 @@
+
+use std::{str::FromStr, env};
+use once_cell::sync::Lazy;
+use zapatos_types::chain_id::NamedChain;
+/// for getting chain config from environment variables
+/// in 0L abscissa apss this will override the 0L.toml file
+/// in vm-genesis this will set the chain configs
+pub const ENV_VAR_MODE_0L: &str = "MODE_0L";
+
+pub static MODE_0L: Lazy<NamedChain> = Lazy::new(|| {
+  let st = env::var(ENV_VAR_MODE_0L)
+    .unwrap_or("MAINNET".to_string());
+  NamedChain::from_str(st.to_uppercase().as_str())
+    .unwrap_or(NamedChain::MAINNET)
+});
