ff16d83091059a685d3243728bca2fc6ada3df7a
root <1364012+0o-de-lally@users.noreply.github.com>
2023-06-01 19:53:18 +0000

    update genesis to save correct host and port

---
+++ util/dev-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Ubuntu
+apt-get update
+
+apt install -y build-essential lld pkg-config libssl-dev libclang-dev
+
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+snap install sccache
+
+echo [build]$'\n'rustc-wrapper = $(which sccache) > $HOME/.cargo/config.toml 
