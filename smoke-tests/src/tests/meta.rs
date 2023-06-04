98bacd7817ec08602469b9d8c119f6941dbc11e5
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-06-04 12:31:55 -0400

    test that an end user can migrate balance

--- smoke-tests/src/tests/meta.rs
+++ smoke-tests/src/tests/meta.rs
@@ -1,6 +1,5 @@
 use aptos_smoke_test::smoke_test_environment::{
   new_local_swarm_with_release,
-  new_local_swarm_with_aptos,
 };
 use libra_framework::release::ReleaseTarget;
 use aptos_forge::Swarm;
@@ -9,7 +8,7 @@ use aptos_forge::Swarm;
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 async fn _meta_can_start_swarm() {
 
-    // let release = ReleaseTarget::Head.load_bundle().unwrap();
-    let mut swarm = new_local_swarm_with_aptos(4).await;
+    let release = ReleaseTarget::Head.load_bundle().unwrap();
+    let mut swarm = new_local_swarm_with_release(4, release).await;
     let _info = swarm.aptos_public_info();
 }
