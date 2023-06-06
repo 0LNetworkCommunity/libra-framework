04ed73ae1e1fe1560ab3fd9c247926edd01d5f9c
root <1364012+0o-de-lally@users.noreply.github.com>
2023-06-06 21:25:47 +0000

    wip

--- tools/txs/src/txs_cli/submit_transaction.rs
+++ tools/txs/src/txs_cli/submit_transaction.rs
@@ -2,9 +2,18 @@ use anyhow::Result;
 use libra_config::extension::client_ext::ClientExt;
 use libra_txs::{rest_client::Client, types::transaction::SignedTransaction};
 
-pub async fn run(signed_trans: &SignedTransaction) -> Result<()> {
+pub async fn submit(signed_trans: &SignedTransaction) -> Result<()> {
     let client = Client::default()?;
     let pending_trans = client.submit(signed_trans).await?.into_inner();
-    client.wait_for_transaction(&pending_trans).await?;
+    let res = client.wait_for_transaction(&pending_trans).await?;
+
+    match res.inner().success() {
+        true => {
+          println!("transaction success!")
+        },
+        false => {
+          println!("transaction not successful, status: {:?}", &res.inner().vm_status());
+        },
+    }
     Ok(())
 }
