d9eca3cfd1f0ae4039c7096fb8454c08c8718dca
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-06-09 21:05:48 -0400

    proposal script build

--- framework/src/main.rs
+++ framework/src/main.rs
@@ -5,7 +5,7 @@ use clap::Parser;
 use zapatos_types::account_address::AccountAddress;
 use std::path::PathBuf;
 // use libra_framework::builder::release_config_ext;
-use libra_framework::builder::framework_generate_upgrade_proposal::libra_compile_script;
+use libra_framework::builder::framework_generate_upgrade_proposal::{libra_compile_script, init_move_dir_wrapper};
 
 #[derive(Parser)]
 #[clap(name = "libra-framework", author, version, propagate_version = true)]
@@ -78,11 +78,23 @@ impl UpgradeRelease {
         let bundle = framework_release_bundle::ReleaseBundle::read(path)?;
         // dbg!(&bundle);
         // let release_package = bundle.release_package;
-        bundle.generate_script_proposal_impl(AccountAddress::ONE, self.output_dir.clone(), None)?;
+        let script_name = "test_script";
+        let new_path = self.output_dir.join(script_name);
+        dbg!(&new_path);
+        std::fs::create_dir_all(&new_path)?;
+        init_move_dir_wrapper(new_path.clone(), script_name, self.framework_local_dir.clone())?;
+
+        let new_file_name = new_path.join("sources").join(&format!("{}.move", script_name));
+        dbg!(&new_file_name);
+        bundle.generate_script_proposal_impl(AccountAddress::ONE, new_file_name, None)?;
         dbg!("ok");
-        let hash = libra_compile_script(&self.output_dir)?;
+        
+        let (bytes, hash) = libra_compile_script(&new_path)?;
+
+        std::fs::write(new_path.join("script.mv"), bytes)?;
+        std::fs::write(new_path.join("script_sha3"), hash.to_string().as_bytes())?;
         // let hash = release_config_ext::libra_generate_hash(self.output_dir.clone(), self.framework_local_dir)?;
-        println!("hash: {:?}", hash.1);
+        println!("hash: {:?}", hash);
         Ok(())
       } else {
         let release_cfg = libra_release_cfg_default();
