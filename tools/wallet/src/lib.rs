d6b27920d64d7b14d174417202bc7eb09bd0921b
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-06-27 13:22:25 -0400

    cleanup

--- tools/wallet/src/lib.rs
+++ tools/wallet/src/lib.rs
@@ -1,13 +1,14 @@
 // #![warn(unused_crate_dependencies)]
 // Unused crate dependencies is not reliable. Uncomment to run check or use `cargo machete`
 
+pub mod account_keys;
 pub mod keys;
 pub mod key_gen;
-pub mod account_keys;
+pub mod legacy;
+pub mod load_keys;
+pub mod scheme;
 pub mod utils;
 pub mod validator_files;
 pub mod wallet_cli;
 
 
-pub mod scheme;
-pub mod load_keys;
