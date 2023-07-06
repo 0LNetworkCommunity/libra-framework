9eb8566b9435ea6ebe6c2b47026963b5d5293e9a
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-07-06 19:33:44 -0400

    update fixtures

--- tools/genesis/src/supply.rs
+++ tools/genesis/src/supply.rs
@@ -18,7 +18,6 @@ pub struct Supply {
   pub donor_directed: u64,
 }
 
-
 fn inc_supply(mut acc: Supply, r: &LegacyRecovery, dd_wallet_list: &Vec<LegacyAddress>) -> anyhow::Result<Supply> {
 
     // get balances
@@ -50,15 +49,21 @@ fn inc_supply(mut acc: Supply, r: &LegacyRecovery, dd_wallet_list: &Vec<LegacyAd
       }
 
 
+    } else if r.cumulative_deposits.is_some() { 
+      // catches the cases of any dd wallets that were mapped to slow wallets
+      acc.slow_locked = acc.slow_locked.checked_add(amount).unwrap();
+      acc.slow_total = acc.slow_total.checked_add(amount).unwrap();
     } else {
+      
       acc.normal = acc.normal.checked_add(amount).unwrap();
     }
     Ok(acc)
 }
 
 /// iterate over the recovery file and get the sum of all balances.
+/// there's an option to map certain donor-directed wallets to be counted as slow wallets
 /// Note: this may not be the "total supply", since there may be coins in other structs beside an account::balance, e.g escrowed in contracts.
-pub fn get_supply_struct(rec: &Vec<LegacyRecovery>) -> anyhow::Result<Supply> {
+pub fn get_supply_struct(rec: &Vec<LegacyRecovery>, map_dd_to_slow: Vec<LegacyAddress>) -> anyhow::Result<Supply> {
   let zeroth = Supply {
     total: 0,
     normal: 0,
@@ -77,8 +82,13 @@ pub fn get_supply_struct(rec: &Vec<LegacyRecovery>) -> anyhow::Result<Supply> {
     .as_ref()
     .context("could not find list of community wallets")?;
 
+  let dd_list: Vec<LegacyAddress> = dd_wallets.clone().list
+      .into_iter()
+      .filter(|e| !map_dd_to_slow.contains(e))
+      .collect();
+
   rec.iter().try_fold(zeroth, |acc, r| {
-    inc_supply(acc, r, &dd_wallets.list)
+    inc_supply(acc, r, &dd_list)
   })
 }
 
@@ -90,13 +100,39 @@ fn test_get_struct() {
     
     let r = crate::parse_json::parse(p).unwrap();
 
-    let supply = get_supply_struct(&r).unwrap();
+    let addr_to_map =  vec![
+    "2B0E8325DEA5BE93D856CFDE2D0CBA12".parse::<LegacyAddress>().unwrap(),
+    "3A6C51A0B786D644590E8A21591FA8E2".parse::<LegacyAddress>().unwrap()
+    ];
+
+    let supply = get_supply_struct(&r, addr_to_map).unwrap();
     dbg!(&supply);
+    let pct_normal= supply.normal as f64 / supply.total as f64;
+    dbg!(&pct_normal);
     let pct_slow = supply.slow_total as f64 / supply.total as f64;
     dbg!(&pct_slow);
     let pct_dd = supply.donor_directed as f64 / supply.total as f64;
     dbg!(&pct_dd);
     let pct_val_locked = supply.validator_locked as f64 / supply.total as f64;
     dbg!(&pct_val_locked);
+
+    let  sum_types = pct_normal + pct_slow + pct_dd;
+    assert!(sum_types == 1.0);
+    dbg!(sum_types);
     assert!(supply.total == 2397436809784621);
+
+    // future uses would equal 70% in this scenario.
+    let future = supply.total * 70 / 100;
+    dbg!(&future);
+    let remainder = future - supply.donor_directed;
+    dbg!("{#:}", remainder);
+    let ratio: f64 = remainder as f64 / supply.validator_locked as f64;
+    dbg!(&ratio);
+
+    let to_escrow = ratio * supply.validator_locked as f64;
+    dbg!(&to_escrow);
+    let sum = to_escrow as u64 + (supply.slow_total - to_escrow as u64) + supply.normal + supply.donor_directed;
+    dbg!(&sum);
+    assert!(supply.total == sum);
+
 }
