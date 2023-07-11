a6d18a255133bd8e13b2e8d0baa82b2bca7bb655
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-07-11 13:52:02 -0400

    cleanup entry point args

--- tools/txs/src/txs_cli.rs
+++ tools/txs/src/txs_cli.rs
@@ -1,15 +1,18 @@
+use std::path::PathBuf;
+
 use crate::submit_transaction::Sender;
 
 use anyhow::Result;
 use clap::Parser;
 use indoc::indoc;
+use libra_types::legacy_types::app_cfg::AppCfg;
 use libra_wallet::account_keys::{get_keys_from_prompt, get_keys_from_mnem};
 use url::Url;
 
 use zapatos_sdk::rest_client::Client;
 use zapatos_sdk::types::{AccountKey, account_address::AccountAddress};
 use zapatos_sdk::crypto::{ValidCryptoMaterialStringExt, ed25519::Ed25519PrivateKey};
-use zapatos_types::chain_id::ChainId;
+use zapatos_types::chain_id::{NamedChain, ChainId};
 
 #[derive(Parser)]
 #[clap(name = env!("CARGO_PKG_NAME"), author, version, about, long_about = None, arg_required_else_help = true)]
@@ -18,24 +21,32 @@ pub struct TxsCli {
       #[clap(subcommand)]
       pub subcommand: Option<TxsSub>,
 
-      /// Optional mnemonic to pass at runtime. Otherwise this will prompt for mnemonic.
+      /// optional, path to the config file
+      #[clap(short, long)]
+      pub config_path: Option<PathBuf>,
+
+      /// optional, mnemonic to pass at runtime. Otherwise this will prompt for mnemonic.
       #[clap(short, long)]
       pub mnemonic: Option<String>,
 
-      /// Private key of the account. Otherwise this will prompt for mnemonic. Warning: intended for testing.
+      /// optional, private key of the account. Otherwise this will prompt for mnemonic. Warning: intended for testing.
       #[clap(short, long)]
       pub test_private_key: Option<String>,
 
-      /// URL of the upstream node to send tx to, including port
+      /// optional, id of chain as name. Will default to MAINNET;
+      #[clap(short, long)]
+      pub chain_id: Option<NamedChain>,
+
+      /// optional, URL of the upstream node to send tx to, including port
       /// Otherwise will default to what is in the config file
       #[clap(short, long)]
       pub url: Option<Url>,
 
-      /// Maximum number of gas units to be used to send this transaction
+      /// optional, maximum number of gas units to be used to send this transaction
       #[clap(short, long)]
       pub max_gas: Option<u64>,
 
-      /// The amount of coins to pay for 1 gas unit. The higher the price is, the higher priority your transaction will be executed with
+      /// optional, the amount of coins to pay for 1 gas unit. The higher the price is, the higher priority your transaction will be executed with
       #[clap(short, long)]
       pub gas_unit_price: Option<u64>,
 }
@@ -85,7 +96,7 @@ pub enum TxsSub {
             help = indoc!{ r#"
                 Type arguments separated by commas
 
-                Example: 
+                Example:
                 'u8, u16, u32, u64, u128, u256, bool, address, vector<u8>, signer'
                 '0x1::aptos_coin::AptosCoin'
             "#}
@@ -141,7 +152,7 @@ pub enum TxsSub {
             help = indoc!{ r#"
                 Type arguments separated by commas
 
-                Example: 
+                Example:
                 'u8, u16, u32, u64, u128, u256, bool, address, vector<u8>, signer'
                 '0x1::aptos_coin::AptosCoin'
             "#}
@@ -173,14 +184,24 @@ impl TxsCli {
         } else {
           Ed25519PrivateKey::from_encoded_string(&self.test_private_key.as_ref().unwrap())?
         };
-        
-        let client_opt = if self.url.is_some() {
-          Some(Client::new(self.url.as_ref().unwrap().to_owned()))
-        } else { None };
+
+        let chain_name = self.chain_id.unwrap_or(NamedChain::MAINNET);
+
+        let url = if let Some(u) = self.url.as_ref() {
+            u.to_owned()
+        }else {
+          AppCfg::load(self.config_path.clone())?.pick_url(Some(chain_name))?
+        };
+
+        let client = Client::new(url);
 
         // TODO: load profile from ConfigCli::load_ext
-        let mut send = Sender::new(AccountKey::from_private_key(pri_key), ChainId::test(), client_opt).await?;
-        
+        let mut send = Sender::new(
+          AccountKey::from_private_key(pri_key),
+          ChainId::new(chain_name.id()),
+          Some(client),
+        ).await?;
+
         match &self.subcommand {
             Some(TxsSub::CreateAccount {
                 account_address,
