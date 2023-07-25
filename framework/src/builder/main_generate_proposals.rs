10789fc5545d7c0065576ffa34907dc1bfa88960
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-07-25 13:03:53 -0400

    cleanup framework main()

--- framework/src/builder/main_generate_proposals.rs
+++ framework/src/builder/main_generate_proposals.rs
@@ -5,6 +5,7 @@ use std::path::PathBuf;
 use zapatos_release_builder::ReleaseConfig;
 
 
+// TODO: Deprecate this method since we now start from an mrb file?
 pub fn run(release_cfg: ReleaseConfig, output_dir: PathBuf, framework_local_dir: PathBuf) -> anyhow::Result<()> {
   release_cfg.libra_generate_release_proposal_scripts(output_dir.as_path(), framework_local_dir)
 }
--- framework/src/main.rs
+++ framework/src/main.rs
@@ -2,7 +2,6 @@
 
 use anyhow::Context;
 use clap::Parser;
-// use zapatos_crypto::HashValue;
 use std::path::PathBuf;
 
 use libra_framework::{
@@ -13,8 +12,6 @@ use libra_framework::{
 
 use zapatos_framework::ReleaseBundle;
 use zapatos_types::account_address::AccountAddress;
-
-// use libra_framework::builder::release_config_ext;
 
 #[derive(Parser)]
 #[clap(name = "libra-framework", author, version, propagate_version = true)]
@@ -56,79 +53,77 @@ impl GenesisRelease {
 }
 
 /// Creates all artifacts for a network governance upgrade
-/// NOTE: this is an 0L reconstruction of vendor apis.
+// NOTE: this is an 0L reconstruction of vendor apis.
 #[derive(Debug, Parser)]
 struct UpgradeRelease {
     /// dir to save all the artifacts for the release.
     #[clap(short, long)]
     output_dir: PathBuf,
-    /// directory of the framework source code
-    /// usually ./framework
+
+    /// directory of the framework source code. Usually `./framework/lbra-framework`
     #[clap(short, long)]
     framework_local_dir: PathBuf,
 
-    /// provide a prebuilt release framework.mrb
+    /// provide a prebuilt release framework.mrb. Usually `./framework/releases/head.mrb
     #[clap(short, long)]
     mrb_path: PathBuf,
 
-    /// TODO: optionally pass a config file with the release config
-    /// if there are parameter or raw script changes.
-    #[clap(short, long)]
-    release_config: Option<PathBuf>,
-
+    // TODO: optionally pass a config file with the release config
+    // /// if there are parameter or raw script changes.
+    // #[clap(short, long)]
+    // release_config: Option<PathBuf>,
 
 }
 
 impl UpgradeRelease {
     fn execute(self) -> anyhow::Result<()> {
-        // we are usually only interested in upgrading the framework
-        // dbg!(&self.mrb_path);
       let script_name = "framework_upgrade";
       let package_dir = self.output_dir.join(script_name);
-      // if let Some(path) = self.mrb_path {
-            // assert!(path.exists(), "no .mrb bundle at this address {path:?}");
 
-            println!("preparing upgrade Move package from prebuilt framework at: {:?}", &self.mrb_path);
+      println!("preparing upgrade Move package from prebuilt framework at: {:?}", &self.mrb_path);
 
-            let bundle = ReleaseBundle::read(self.mrb_path.clone()).context("could not read a bundle release")?;
+      let bundle = ReleaseBundle::read(self.mrb_path.clone()).context("could not read a bundle release")?;
 
 
-            dbg!(&package_dir);
-            std::fs::create_dir_all(&package_dir)
-                .context("could not create the output directory {new_path:?}")?;
+      dbg!(&package_dir);
+      std::fs::create_dir_all(&package_dir)
+          .context("could not create the output directory {new_path:?}")?;
 
-            // TODO: rename this. init_move_package_with_local_framework
-            init_move_dir_wrapper(
-                package_dir.clone(),
-                script_name,
-                self.framework_local_dir.clone(),
-            )?;
+      // TODO: rename this. init_move_package_with_local_framework
+      init_move_dir_wrapper(
+          package_dir.clone(),
+          script_name,
+          self.framework_local_dir.clone(),
+      )?;
 
-            let tx_script_filename = package_dir
-                .join("sources")
-                .join(&format!("{}.move", script_name));
-            dbg!(&tx_script_filename);
-            libra_generate_script_proposal_impl(&bundle, AccountAddress::ONE, tx_script_filename, None)?;
-            dbg!("ok");
+      let tx_script_filename = package_dir
+          .join("sources")
+          .join(&format!("{}.move", script_name));
+      dbg!(&tx_script_filename);
+      libra_generate_script_proposal_impl(&bundle, AccountAddress::ONE, tx_script_filename, None)?;
+      dbg!("ok");
 
-            println!("compiling script");
-            let (bytes, hash) = libra_compile_script(&package_dir)?;
+      println!("compiling script");
+      let (bytes, hash) = libra_compile_script(&package_dir)?;
 
-            std::fs::write(package_dir.join("script.mv"), bytes)?;
-            std::fs::write(package_dir.join("script_sha3"), hash.to_string().as_bytes())?;
+      std::fs::write(package_dir.join("script.mv"), bytes)?;
+      std::fs::write(package_dir.join("script_sha3"), hash.to_string().as_bytes())?;
 
-            println!("success: upgrade script built at: {:?}", package_dir);
-            println!("hash: {:?}", hash);
+      println!("success: upgrade script built at: {:?}", package_dir);
+      println!("hash: {:?}", hash.to_hex_literal());
 
-        // else {
+      Ok(())
 
-        //     let release_cfg = libra_release_cfg_default();
-        //     match main_generate_proposals::run(release_cfg, package_dir, self.framework_local_dir) {
-        //         Ok(_) => HashValue::random(),
-        //         Err(e) => bail!("could not create releas build, message: {}", &e.to_string()),
-        //     }
-        // };
+      // DEPRECATION NOTICE
+      // We don't need to build the framework mrb in this step. We assume that it was done previously. Future devs can decide if we provide the option to rebuild in a single step.
 
-        Ok(())
+      //     let release_cfg = libra_release_cfg_default();
+      //     match main_generate_proposals::run(release_cfg, package_dir, self.framework_local_dir) {
+      //         Ok(_) => HashValue::random(),
+      //         Err(e) => bail!("could not create releas build, message: {}", &e.to_string()),
+      //     }
+      // };
+
+
     }
 }
