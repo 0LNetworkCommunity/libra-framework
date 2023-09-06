1883b80a50a0953d2f504e50baab3f5d83c7d46a
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-09-06 17:47:04 +0000

    patched txs transfer tests

--- tools/txs/tests/transfer.rs
+++ tools/txs/tests/transfer.rs
@@ -1,28 +1,38 @@
-use libra_smoke_tests::libra_smoke::LibraSmoke;
+use libra_smoke_tests::{libra_smoke::LibraSmoke, configure_validator};
 use libra_txs::txs_cli::{TxsCli, TxsSub::Transfer};
 
 // Testing that we can send the minimal transaction: a transfer from one existing validator to another.
 // Case 1: send to an existing account: another genesis validator
 // Case 2: send to an account which does not yet exist, and the account gets created on chain.
 
-
 /// Case 1: send to an existing account: another genesis validator
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 async fn smoke_transfer_exists() {
-    let s = LibraSmoke::new(Some(2)).await.expect("can't start swarm");
+    let d = diem_temppath::TempPath::new();
+
+    let mut ls = LibraSmoke::new(Some(2))
+        .await
+        .expect("could not start libra smoke");
+
+    let (_, _app_cfg) =
+        configure_validator::init_val_config_files(&mut ls.swarm, 0, d.path().to_owned())
+            .await
+            .expect("could not init validator config");
+
+    // let s = LibraSmoke::new(Some(2)).await.expect("can't start swarm");
 
     // 1. simple case: account already exitsts
-    let recipient = s.swarm.validators().nth(1).unwrap().peer_id(); // sending to second genesis node.
+    let recipient = ls.swarm.validators().nth(1).unwrap().peer_id(); // sending to second genesis node.
     let cli = TxsCli {
         subcommand: Some(Transfer {
             to_account: recipient,
             amount: 1,
         }),
         mnemonic: None,
-        test_private_key: Some(s.encoded_pri_key.clone()),
+        test_private_key: Some(ls.encoded_pri_key.clone()),
         chain_id: None,
-        config_path: None,
-        url: Some(s.api_endpoint.clone()),
+        config_path: Some(d.path().to_owned().join("libra.yaml")),
+        url: Some(ls.api_endpoint.clone()),
         gas_max: None,
         gas_unit_price: None,
     };
@@ -31,22 +41,22 @@ async fn smoke_transfer_exists() {
         .await
         .expect("cli could not send to existing account");
 
-    // //2. Account does not yet exist.
-    // cli.subcommand = Some(Transfer {
-    //     to_account: s.marlon_rando().address(),
-    //     amount: 1,
-    // });
-
-    // cli.run().await.expect("cli could not create new account");
-
-    //TODO: check the balance
 }
 
 
 /// Case 2: send to an account which does not yet exist, and the account gets created on chain.
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 async fn smoke_transfer_create() {
-    let mut s = LibraSmoke::new(Some(2)).await.expect("can't start swarm");
+    let d = diem_temppath::TempPath::new();
+
+    let mut s = LibraSmoke::new(Some(2))
+        .await
+        .expect("could not start libra smoke");
+
+    let (_, _app_cfg) =
+        configure_validator::init_val_config_files(&mut s.swarm, 0, d.path().to_owned())
+            .await
+            .expect("could not init validator config");
 
     // case 2. Account does not yet exist.
     let cli = TxsCli {
@@ -57,7 +67,7 @@ async fn smoke_transfer_create() {
         mnemonic: None,
         test_private_key: Some(s.encoded_pri_key.clone()),
         chain_id: None,
-        config_path: None,
+        config_path: Some(d.path().to_owned().join("libra.yaml")),
         url: Some(s.api_endpoint.clone()),
         gas_max: None,
         gas_unit_price: None,
