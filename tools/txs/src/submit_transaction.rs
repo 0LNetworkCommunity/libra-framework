b62184430577b90797c211ad7050285701b1e093
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-09-07 21:53:17 +0000

    cargo fmt

--- tools/txs/src/submit_transaction.rs
+++ tools/txs/src/submit_transaction.rs
@@ -251,8 +251,8 @@ impl Sender {
             .as_secs();
         let time = t + DEFAULT_TIMEOUT_SECS * 10;
         let tb = TransactionBuilder::new(payload, time, self.chain_id)
-        .gas_unit_price(self.tx_cost.coin_price_per_unit)
-        .max_gas_amount(self.tx_cost.max_gas_unit_for_tx);
+            .gas_unit_price(self.tx_cost.coin_price_per_unit)
+            .max_gas_amount(self.tx_cost.max_gas_unit_for_tx);
         self.local_account.sign_with_transaction_builder(tb)
     }
 
