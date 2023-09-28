a2408c05c5ae6d02c6129b5211c78ceb823a2925
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-09-28 17:26:08 +0000

    fmt

--- smoke-tests/src/tests/meta.rs
+++ smoke-tests/src/tests/meta.rs
@@ -4,7 +4,6 @@ use diem_forge::Swarm;
 use libra_framework::release::ReleaseTarget;
 use smoke_test::smoke_test_environment::new_local_swarm_with_release;
 
-
 /// Testing that we can get a swarm up with the current head.mrb
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 async fn meta_can_start_swarm() {
@@ -13,8 +12,9 @@ async fn meta_can_start_swarm() {
     let mut info = swarm.diem_public_info();
 
     let a = info.random_account();
-    info.create_user_account(a.public_key()).await.expect("cannot create account");
-
+    info.create_user_account(a.public_key())
+        .await
+        .expect("cannot create account");
 }
 
 /// testing the LibraSmoke abstraction can load
