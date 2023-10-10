d0fcd12ebc6050fb7554d745cc387734a9aecf7e
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-10-10 21:27:21 +0000

    test for sync_only passing

--- tools/rescue/tests/genesis.rs
+++ tools/rescue/tests/genesis.rs
@@ -1,24 +1,18 @@
+use libra_smoke_tests::libra_smoke::LibraSmoke;
 use rescue::{diem_db_bootstrapper::BootstrapOpts, rescue_tx::RescueTxOpts};
-// Copyright © Diem Foundation
-// Parts of the project are originally copyright © Meta Platforms, Inc.
-// SPDX-License-Identifier: Apache-2.0
-#[cfg(test)]
 use smoke_test::{
-    smoke_test_environment::SwarmBuilder,
     storage::{db_backup, db_restore},
     test_utils::{
-        check_create_mint_transfer_node, swarm_utils::insert_waypoint, MAX_CATCH_UP_WAIT_SECS,
+MAX_CATCH_UP_WAIT_SECS,
         MAX_CONNECTIVITY_WAIT_SECS, MAX_HEALTHY_WAIT_SECS,
     },
-    // workspace_builder,
-    // workspace_builder::workspace_root,
 };
 use anyhow::anyhow;
 use diem_config::config::{InitialSafetyRulesConfig, NodeConfig};
 use diem_forge::{get_highest_synced_version, LocalNode, Node, NodeExt, SwarmExt, Validator};
 use diem_logger::prelude::*;
 use diem_temppath::TempPath;
-use diem_types::{transaction::Transaction, waypoint::Waypoint};
+use diem_types::{transaction::Transaction, waypoint::Waypoint, account_address::AccountAddress};
 use move_core_types::language_storage::CORE_CODE_ADDRESS;
 use regex::Regex;
 use std::{
@@ -63,6 +57,50 @@ async fn wait_for_node(validator: &mut dyn Validator, expected_to_connect: usize
 }
 
 #[tokio::test]
+/// Testing sync only
+async fn test_sync_only_mode() {
+
+    let num_nodes: usize = 5;
+
+    let mut s = LibraSmoke::new(Some(num_nodes as u8))
+        .await
+        .expect("could not start libra smoke");
+
+    let env = &mut s.swarm;
+
+    println!("1. Set sync_only = true for the last node and check it can sync to others");
+    let node = env.validators_mut().nth(4).unwrap();
+    let mut new_config = node.config().clone();
+    new_config.consensus.sync_only = true;
+    update_node_config_restart(node, new_config.clone());
+    wait_for_node(node, num_nodes - 1).await;
+    // wait for some versions
+    env.wait_for_all_nodes_to_catchup_to_version(10, Duration::from_secs(MAX_CATCH_UP_WAIT_SECS))
+        .await
+        .expect("could not catc up to version 10");
+
+    println!("2. Set sync_only = true for all nodes and restart");
+    for node in env.validators_mut() {
+        let mut node_config = node.config().clone();
+        node_config.consensus.sync_only = true;
+        update_node_config_restart(node, node_config);
+        wait_for_node(node, num_nodes - 1).await;
+    }
+
+    println!("3. delete one node's db and test they can still sync when sync_only is true for every nodes");
+    let node = env.validators_mut().nth(3).unwrap();
+    node.stop();
+    node.clear_storage().await.unwrap();
+    node.start().expect("could not restart node");
+
+    println!("4. verify all nodes are at the same round and no progress being made");
+    env.wait_for_all_nodes_to_catchup(Duration::from_secs(MAX_CATCH_UP_WAIT_SECS))
+        .await
+        .unwrap();
+
+}
+
+#[tokio::test]
 /// This test verifies the flow of a genesis transaction after the chain starts.
 /// 1. Test the consensus sync_only mode, every node should stop at the same version.
 /// 2. Test the db-bootstrapper applying a manual genesis transaction (remove validator 0) on diemdb directly
@@ -77,26 +115,28 @@ async fn test_genesis_transaction_flow() {
 
     // println!("0. pre-building finished.");
 
-    let num_nodes = 5;
+    let num_nodes: usize = 5;
     // let (mut env, cli, _) = SwarmBuilder::new_local(num_nodes)
     //     .with_diem()
     //     .build_with_cli(0)
     //     .await;
 
-    let mut s = LibraSmoke::new(Some(num_nodes))
+    let mut s = LibraSmoke::new(Some(num_nodes as u8))
         .await
         .expect("could not start libra smoke");
 
-    // println!("1. Set sync_only = true for the last node and check it can sync to others");
-    // let node = env.validators_mut().nth(4).unwrap();
-    // let mut new_config = node.config().clone();
-    // new_config.consensus.sync_only = true;
-    // update_node_config_restart(node, new_config.clone());
-    // wait_for_node(node, num_nodes - 1).await;
-    // // wait for some versions
-    // env.wait_for_all_nodes_to_catchup_to_version(10, Duration::from_secs(MAX_CATCH_UP_WAIT_SECS))
-    //     .await
-    //     .unwrap();
+    let env = &mut s.swarm;
+
+    println!("1. Set sync_only = true for the last node and check it can sync to others");
+    let node = env.validators_mut().nth(4).unwrap();
+    let mut new_config = node.config().clone();
+    new_config.consensus.sync_only = true;
+    update_node_config_restart(node, new_config.clone());
+    wait_for_node(node, num_nodes - 1).await;
+    // wait for some versions
+    env.wait_for_all_nodes_to_catchup_to_version(10, Duration::from_secs(MAX_CATCH_UP_WAIT_SECS))
+        .await
+        .unwrap();
 
     // println!("2. Set sync_only = true for all nodes and restart");
     // for node in env.validators_mut() {
@@ -343,5 +383,5 @@ fn make_script(first_validator_address: AccountAddress) -> PathBuf{
     let genesis_blob_path = TempPath::new();
     genesis_blob_path.create_as_file().unwrap();
 
-    genesis_blob_path
+    genesis_blob_path.path().to_owned()
 }
