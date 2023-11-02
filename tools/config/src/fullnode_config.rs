ab76ef071e2d1825ba02c21a619ebe5b81c01fd6
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-11-02 21:23:18 +0000

    updating nodeconfig file with seed peers

--- tools/config/src/fullnode_config.rs
+++ tools/config/src/fullnode_config.rs
@@ -1,17 +1,32 @@
-use std::{collections::HashMap, path::PathBuf};
-
+use std::{collections::HashMap, path::{PathBuf, Path}};
+use diem_config::config::NodeConfig;
 use diem_types::{PeerId, network_address::NetworkAddress, waypoint::Waypoint};
 
 
+pub fn add_peers_to_yaml(path: &Path, peers: HashMap<PeerId, Vec<NetworkAddress>> ) -> anyhow::Result<()> {
+  let string = std::fs::read_to_string(path)?;
+  let mut parsed: NodeConfig = serde_yaml::from_str(&string)?;
+
+  parsed.full_node_networks.iter_mut().for_each(move |e| {
+    if e.network_id.is_public_network() {
+      e.seed_addrs = peers.clone();
+    }
+  });
+
+  let ser = serde_yaml::to_string(&parsed)?;
+  std::fs::write(path, ser)?;
+
+  Ok(())
+}
 
 /// download genesis blob
 ///
-pub fn download_genesis(home_dir: Option<PathBuf>) {
+pub fn download_genesis(_home_dir: Option<PathBuf>) {
 
 }
 
 /// get seed peers from an upstream url
-pub async fn fetch_seed_addresses(home_dir: Option<PathBuf>, url: Option<&str>) -> anyhow::Result<HashMap<PeerId, Vec<NetworkAddress>>> {
+pub async fn fetch_seed_addresses(url: Option<&str>) -> anyhow::Result<HashMap<PeerId, Vec<NetworkAddress>>> {
     let u = url.unwrap_or("https://raw.githubusercontent.com/0LNetworkCommunity/seed-peers/main/seed_peers.yaml");
     let res = reqwest::get(u).await?;
     let out = res.text().await?;
@@ -34,22 +49,16 @@ base:
     from_config: '{waypoint}'
 
 execution:
-  genesis_file_location: '{path}/genesis/genesis.blob'
+  genesis_file_location: '{path}/genesis.blob'
 
 state_sync:
      state_sync_driver:
         bootstrapping_mode: DownloadLatestStates
         continuous_syncing_mode: ExecuteTransactionsOrApplyOutputs
 
-execution:
-  genesis_file_location: '{path}/genesis.blob'
-
 full_node_networks:
 - network_id: 'public'
   listen_address: '/ip4/0.0.0.0/tcp/6182'
-  seed_addrs:
-      3c37c7d6a5122a6b9ef07a11cc40e445874eb0841ae028d6326bf67768cce235:
-        - '/ip4/204.186.74.42/tcp/6182/noise-ik/0x3c37c7d6a5122a6b9ef07a11cc40e445874eb0841ae028d6326bf67768cce235/handshake/0'
 
 api:
   enabled: true
@@ -62,7 +71,36 @@ api:
 
 #[tokio::test]
 async fn get_peers() {
-  let seed = fetch_seed_addresses(None, None).await.unwrap();
+  let seed = fetch_seed_addresses(None).await.unwrap();
 
   dbg!(&seed);
 }
+
+#[tokio::test]
+async fn get_yaml() {
+  use std::str::FromStr;
+  let p = diem_temppath::TempPath::new().path().to_owned();
+
+  let seeds = fetch_seed_addresses(None).await.unwrap();
+
+  let y = make_fullnode_yaml(
+    Some(p.clone()),
+    Waypoint::from_str("0:95023f4d6a7e24cac3e52cad29697184db260214210b57aef3f1031ad4d8c02c").unwrap(),
+  ).unwrap();
+
+  std::fs::write(&p, y).unwrap();
+
+  // let mut parsed: NodeConfig = serde_yaml::from_str(&y).unwrap();
+  // parsed.full_node_networks.iter_mut().for_each(move |e| {
+  //   if e.network_id.is_public_network() {
+  //     e.seed_addrs = seeds.clone();
+  //   }
+  // });
+
+  add_peers_to_yaml(&p, seeds).unwrap();
+
+  let text = std::fs::read_to_string(&p).unwrap();
+
+  dbg!(&text);
+}
+
