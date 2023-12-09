b7acf9d3247fb047707037075335494ccb939a30
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-12-09 03:42:03 +0000

    try disabling reconfiguration

--- tools/rescue/src/framework_payload.rs
+++ tools/rescue/src/framework_payload.rs
@@ -5,12 +5,17 @@ use diem_types::{
     transaction::{ChangeSet, WriteSetPayload},
 };
 use move_core_types::{
-    identifier::{IdentStr, Identifier},
+    identifier::Identifier,
     language_storage::ModuleId,
     value::MoveValue,
 };
 use move_vm_test_utils::gas_schedule::GasStatus;
 use std::path::PathBuf;
+
+
+use diem_vm::move_vm_ext::SessionExt;
+use move_core_types::language_storage::TypeTag;
+ use move_core_types::value::serialize_values;
 
 /// generate the writeset of changes from publishing all a framework bundle
 pub async fn stlib_payload(db_path: PathBuf) -> anyhow::Result<WriteSetPayload> {
@@ -20,27 +25,31 @@ pub async fn stlib_payload(db_path: PathBuf) -> anyhow::Result<WriteSetPayload> 
     let new_stdlib = libra_framework::head_release_bundle().legacy_copy_code();
 
     let v = db.get_latest_version().await?;
+
     let cs = db.run_session_at_version(v, |session| {
         let mut gas_status = GasStatus::new_unmetered();
+
+        disable_reconfiguration(session);
         session
             .publish_module_bundle(new_stdlib, CORE_CODE_ADDRESS, &mut gas_status)
             .expect("could not publish framework");
+        enable_reconfiguration(session);
+        // let vm_signer = MoveValue::Signer(AccountAddress::ONE)
+        //     .simple_serialize()
+        //     .expect("get the 0x1 signer bytes");
 
-        let vm_signer = MoveValue::Signer(AccountAddress::ONE)
-            .simple_serialize()
-            .expect("get the 0x1 signer bytes");
-        session
-            .execute_function_bypass_visibility(
-                &ModuleId::new(
-                    "0x1".parse().unwrap(),
-                    Identifier::new("reconfiguration").unwrap(),
-                ),
-                IdentStr::new("reconfigure_for_rescue").unwrap(),
-                vec![],
-                vec![vm_signer],
-                &mut gas_status,
-            )
-            .expect("could not bump rescue epoch");
+        // session
+        //     .execute_function_bypass_visibility(
+        //         &ModuleId::new(
+        //             "0x1".parse().unwrap(),
+        //             Identifier::new("reconfiguration").unwrap(),
+        //         ),
+        //         IdentStr::new("reconfigure_for_rescue").unwrap(),
+        //         vec![],
+        //         vec![vm_signer],
+        //         &mut gas_status,
+        //     )
+        //     .expect("could not bump rescue epoch");
         Ok(())
     })?;
 
@@ -73,3 +82,73 @@ pub async fn execute_script_payload(
     let other_changset_type_fml = ChangeSet::new(ws, events);
     Ok(WriteSetPayload::Direct(other_changset_type_fml))
 }
+
+// NOTE this is adapted from Diem
+// diem-move/writeset-transaction-generator/src/writeset_builder.rs
+
+// helper to execute a session function
+pub fn exec_function_helper(
+    session: &mut SessionExt,
+    module_name: &str,
+    function_name: &str,
+    ty_args: Vec<TypeTag>,
+    args: Vec<Vec<u8>>,
+) {
+    session
+        .execute_function_bypass_visibility(
+            &ModuleId::new(
+                AccountAddress::ONE,
+                Identifier::new(module_name).unwrap(),
+            ),
+            &Identifier::new(function_name).unwrap(),
+            ty_args,
+            args,
+            &mut GasStatus::new_unmetered(),
+        )
+        .unwrap_or_else(|e| {
+            panic!(
+                "Error calling {}.{}: {}",
+                module_name,
+                function_name,
+                e.into_vm_status()
+            )
+        });
+}
+
+fn disable_reconfiguration(session: &mut SessionExt) {
+    exec_function_helper(
+        session,
+        "Reconfiguration",
+        "disable_reconfiguration",
+        vec![],
+        serialize_values(&vec![MoveValue::Signer(AccountAddress::ONE)]),
+    );
+  }
+
+
+fn enable_reconfiguration(session: &mut SessionExt) {
+    exec_function_helper(
+        session,
+        "Reconfiguration",
+        "enable_reconfiguration",
+        vec![],
+        serialize_values(&vec![MoveValue::Signer(AccountAddress::ONE)]),
+    );
+}
+// TODO: this would be easier than the other way of sending a fork script
+//     pub fn exec_script(
+//     &mut self,
+//     sender: AccountAddress,
+//     script: &Script,
+// ) -> SerializedReturnValues {
+//     let mut temp = vec![sender.to_vec()];
+//     temp.extend(convert_txn_args(script.args()));
+//     self.0
+//         .execute_script(
+//             script.code().to_vec(),
+//             script.ty_args().to_vec(),
+//             temp,
+//             &mut UnmeteredGasMeter,
+//         )
+//         .unwrap()
+// }
--- tools/rescue/src/lib.rs
+++ tools/rescue/src/lib.rs
@@ -1,4 +1,3 @@
-// pub mod admin_script_builder;
 pub mod diem_db_bootstrapper;
 pub mod framework_payload;
 pub mod rescue_tx;
