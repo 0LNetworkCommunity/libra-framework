3184126b866700ba89e2f920433af9b5f903bb25
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-01-25 00:56:39 +0000

    add veto tx

--- tools/txs/src/txs_cli_community.rs
+++ tools/txs/src/txs_cli_community.rs
@@ -3,16 +3,14 @@
 use crate::submit_transaction::Sender;
 use diem_types::account_address::AccountAddress;
 use libra_cached_packages::libra_stdlib;
-use libra_types::{
-    exports::{AuthenticationKey, Ed25519PrivateKey},
-    type_extensions::client_ext::ClientExt,
-};
-use libra_wallet::account_keys::get_keys_from_prompt;
 
 #[derive(clap::Subcommand)]
 pub enum CommunityTxs {
     /// Propose a Tx
     Propose(ProposeTx),
+    /// Donors to Donor Voice addresses (like Community Wallets), can vote to
+    /// reject transactions.
+    Veto(VetoTx),
     /// initialize a DonorVoice multisig with the initial admins.
     GovInit(InitTx),
     /// propose a change to the authorities of the DonorVoice multisig
@@ -23,27 +21,30 @@ impl CommunityTxs {
     pub async fn run(&self, sender: &mut Sender) -> anyhow::Result<()> {
         match &self {
             CommunityTxs::Propose(rotate) => match rotate.run(sender).await {
-                Ok(_) => println!("SUCCESS: private key rotated"),
+                Ok(_) => println!("SUCCESS: community wallet transfer proposed"),
                 Err(e) => {
-                    println!("ERROR: could not rotate private key, message: {}", e);
+                    println!("ERROR: community wallet transfer rejected, message: {}", e);
+                }
+            },
+            CommunityTxs::Veto(rotate) => match rotate.run(sender).await {
+                Ok(_) => println!("SUCCESS: veto vote submitted"),
+                Err(e) => {
+                    println!("ERROR: veto vote rejected, message: {}", e);
                 }
             },
             CommunityTxs::GovInit(slow) => match slow.run(sender).await {
-                Ok(_) => println!("SUCCESS: account set to Slow Wallet"),
+                Ok(_) => println!("SUCCESS: community wallet initialized"),
                 Err(e) => {
                     println!(
-                        "ERROR: could set the account to Slow Wallet, message: {}",
+                        "ERROR: could not initialize Community Wallet, message: {}",
                         e
                     );
                 }
             },
             CommunityTxs::GovAdmins(slow) => match slow.run(sender).await {
-                Ok(_) => println!("SUCCESS: account set to Slow Wallet"),
+                Ok(_) => println!("SUCCESS: community wallet admin added"),
                 Err(e) => {
-                    println!(
-                        "ERROR: could set the account to Slow Wallet, message: {}",
-                        e
-                    );
+                    println!("ERROR: could not add admin, message: {}", e);
                 }
             },
         }
@@ -62,6 +63,23 @@ pub struct ProposeTx {
 }
 
 impl ProposeTx {
+    pub async fn run(&self, sender: &mut Sender) -> anyhow::Result<()> {
+        let payload = libra_stdlib::slow_wallet_user_set_slow();
+        sender.sign_submit_wait(payload).await?;
+        Ok(())
+    }
+}
+
+#[derive(clap::Args)]
+pub struct VetoTx {
+    #[clap(short, long)]
+    /// The SlowWallet recipient of funds
+    community_wallet: AccountAddress,
+    /// Proposal number
+    proposal_id: u64,
+}
+
+impl VetoTx {
     pub async fn run(&self, sender: &mut Sender) -> anyhow::Result<()> {
         let payload = libra_stdlib::slow_wallet_user_set_slow();
         sender.sign_submit_wait(payload).await?;
