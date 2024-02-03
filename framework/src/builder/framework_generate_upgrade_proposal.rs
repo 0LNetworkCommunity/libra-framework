e2e313dabeb24afd21ad428a250302696c8b7f58
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-02-03 01:14:26 +0000

    clippy

--- framework/src/builder/framework_generate_upgrade_proposal.rs
+++ framework/src/builder/framework_generate_upgrade_proposal.rs
@@ -1,7 +1,10 @@
 //! generate framework upgrade proposal scripts
 //! see vendor diem-move/framework/src/release_bundle.rs
 
-use crate::{builder::{framework_release_bundle::libra_author_script_file, release::ol_release_default}, BYTECODE_VERSION};
+use crate::{
+    builder::{framework_release_bundle::libra_author_script_file, release::ol_release_default},
+    BYTECODE_VERSION,
+};
 use anyhow::{ensure, Context, Result};
 use diem_crypto::HashValue;
 use diem_framework::{BuildOptions, BuiltPackage, ReleasePackage};
