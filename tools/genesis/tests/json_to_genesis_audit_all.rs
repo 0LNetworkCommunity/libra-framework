aa8beebf1d81d5136849221cdf47e66d05ef79ba
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-04-05 21:16:52 +0000

    add e2e test for dropped accounts.

--- tools/genesis/tests/json_to_genesis_audit_all.rs
+++ tools/genesis/tests/json_to_genesis_audit_all.rs
@@ -1,5 +1,8 @@
 //! Tests for the `make_genesis` binary.
 mod support;
+use diem_state_view::account_with_state_view::AsAccountWithStateView;
+use diem_storage_interface::state_view::LatestDbStateCheckpointView;
+use diem_types::account_view::AccountView;
 use diem_types::chain_id::NamedChain;
 use libra_framework::head_release_bundle;
 use libra_genesis_tools::parse_json::recovery_file_parse;
@@ -8,6 +11,8 @@ use libra_genesis_tools::vm::libra_genesis_default;
 use libra_genesis_tools::{compare, genesis::make_recovery_genesis_from_vec_legacy_recovery};
 use libra_genesis_tools::{genesis_reader, parse_json};
 use libra_types::exports::ChainId;
+use libra_types::legacy_types::legacy_recovery_v6::AccountRole;
+use libra_types::move_resource::gas_coin::GasCoinStoreResource;
 use support::{path_utils::json_path, test_vals};
 
 #[test]
@@ -63,6 +68,7 @@ fn test_correct_supply_arithmetic_all() {
 #[test]
 // test that a genesis blob created from struct, will actually contain the data
 fn test_drop_all() {
+    use libra_types::exports::AccountAddress;
     let genesis_vals = test_vals::get_test_valset(4);
 
     let path = json_path()
@@ -76,8 +82,24 @@ fn test_drop_all() {
     let drop_file = json_path().parent().unwrap().join("drop.json");
     parse_json::drop_accounts(&mut user_accounts, &drop_file).unwrap();
 
-    // get the supply arithmetic so that we can compare outputs
-    let supply_stats = supply::populate_supply_stats_from_legacy(&user_accounts).unwrap();
+    let dead = {
+        user_accounts
+            .iter()
+            .find(|e| {
+                e.account
+                    == Some(
+                        AccountAddress::from_hex_literal("0x0012DD85AA97606DD22B3C9A85585D49")
+                            .unwrap(),
+                    )
+            })
+            .expect("should have this account")
+            .clone()
+    };
+
+    assert!(dead.role == AccountRole::Drop);
+
+    // // get the supply arithmetic so that we can compare outputs
+    // let supply_stats = supply::populate_supply_stats_from_legacy(&user_accounts).unwrap();
 
     let gen_tx = make_recovery_genesis_from_vec_legacy_recovery(
         &mut user_accounts,
@@ -95,21 +117,16 @@ fn test_drop_all() {
     // test dump balances
     // compare::export_account_balances(&user_accounts, &db_rw.reader, json_path().parent().unwrap())
     //     .unwrap();
+    let dead_acct = dead.account.unwrap();
+    // Ok now let's compare to what's on chain
+    let db_state_view = db_rw.reader.latest_state_checkpoint_view().unwrap();
+    let account_state_view = db_state_view.as_account_with_state_view(&dead_acct);
 
-    // audit
-    match compare::compare_recovery_vec_to_genesis_tx(
-        &mut user_accounts,
-        &db_rw.reader,
-        &supply_stats,
-    ) {
-        Ok(list) => {
-            if !list.is_empty() {
-                let len = list.len();
-                let out = json_path().parent().unwrap().join("audit.json");
-                std::fs::write(out, serde_json::to_string_pretty(&list).unwrap()).unwrap();
-                panic!("audit list is not empty, errs: {len}");
-            }
-        }
-        Err(_e) => panic!("error creating comparison"),
-    }
+    let on_chain_balance = account_state_view
+        .get_move_resource::<GasCoinStoreResource>()
+        .expect("should have move resource");
+
+    // there will be no balance struct on this account.
+    assert!(on_chain_balance.is_none());
+
 }
