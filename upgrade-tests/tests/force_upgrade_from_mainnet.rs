0a1471a95060a2f51949db5fbb9fac15c53db65a
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-07-13 15:39:45 -0400

    separate files for forced upgrades

---
+++ upgrade-tests/tests/force_upgrade_from_mainnet.rs
@@ -0,0 +1,31 @@
+mod support;
+
+use libra_framework::release::ReleaseTarget;
+
+/////// TEST ARBITRARY UPGRADES ///////
+// do the same as above, but use the "arbitrary" upgrade policy to force an
+// upgrade.
+//
+/// Force upgrade Libra
+#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+async fn smoke_upgrade_mainnet_force_libra() {
+    support::upgrade_multiple_impl(
+        "upgrade-single-lib-force",
+        vec!["1-libra-framework"],
+        ReleaseTarget::Mainnet,
+    )
+    .await
+    .unwrap();
+}
+
+/// Upgrade all modules
+#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+async fn smoke_upgrade_mainnet_force_multiple() {
+    support::upgrade_multiple_impl(
+        "upgrade-multi-lib-force",
+        vec!["1-move-stdlib", "2-vendor-stdlib", "3-libra-framework"],
+        ReleaseTarget::Mainnet,
+    )
+    .await
+    .unwrap();
+}
