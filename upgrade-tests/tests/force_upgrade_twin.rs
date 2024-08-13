1456c417acc9578e539d63c6189e3567876794fd
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-08-13 17:01:45 -0400

    scaffold upgrade test from snapshot

--- upgrade-tests/tests/force_upgrade_twin.rs
+++ upgrade-tests/tests/force_upgrade_twin.rs
@@ -1,5 +1,7 @@
 mod support;
 
+use std::path::PathBuf;
+
 use anyhow::Context;
 use libra_framework::release::ReleaseTarget;
 use libra_smoke_tests::libra_smoke::LibraSmoke;
@@ -25,5 +27,24 @@ async fn twin_test_all_upgrades_dummy() -> anyhow::Result<()> {
         vec!["1-move-stdlib", "2-vendor-stdlib", "3-libra-framework"],
     )
     .await?;
+    Ok(())
+}
+
+
+/// NOTE: WIP: depends on a restored DB having been created.
+#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+#[ignore]
+async fn twin_test_framework_upgrade() -> anyhow::Result<()> {
+
+    let mut s = LibraSmoke::new_with_target(Some(1), None, ReleaseTarget::Mainnet)
+        .await
+        .context("could not start libra smoke")?;
+
+    // note: this DB needs to be a functioning restored snapshot
+    let p = PathBuf::from("$HOME/.libra/data/db");
+
+    Twin::make_twin_swarm(&mut s, Some(p), false).await?;
+
+    support::upgrade_multiple_impl(&mut s, "upgrade-multi-lib", vec!["3-libra-framework"]).await?;
     Ok(())
 }
