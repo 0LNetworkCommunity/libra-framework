c69c12ada43158fdd8253598ab104750950134c7
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-10-09 15:18:21 -0400

    sqlx works in test harness

--- warehouse/tests/sqlx_meta.rs
+++ warehouse/tests/sqlx_meta.rs
@@ -1,14 +1,55 @@
-use sqlx::{PgPool, Row};
+// in cargo.toml...
+// sqlx = { version = "0.8", features = [ "runtime-tokio", "tls-native-tls", "sqlite", "migrate"] }
+
+use sqlx::{Pool, Sqlite, Row};
 
 #[sqlx::test]
-async fn basic_test(pool: PgPool) -> sqlx::Result<()> {
+async fn basic_test(pool: Pool<Sqlite>) -> anyhow::Result<()> {
     let mut conn = pool.acquire().await?;
 
-    let foo = sqlx::query("SELECT * FROM foo")
-        .fetch_one(&mut conn)
-        .await?;
+    let a = sqlx::query("SELECT * FROM foo").execute(&mut *conn).await;
 
-    assert_eq!(foo.get::<String, _>("bar"), "foobar!");
+    // every time you run this there will be an empty db
+    assert!(a.is_err()); // should error with no table called foo
+
+    let id = sqlx::query(
+        r#"
+          CREATE TABLE foo (
+            contact_id INTEGER PRIMARY KEY,
+            first_name TEXT NOT NULL
+          );
+        "#,
+    )
+    .execute(&mut *conn)
+    .await?
+    .last_insert_rowid();
+
+    assert!(id == 0);
+
+    let id = sqlx::query(
+        r#"
+          INSERT INTO foo (contact_id, first_name)
+          VALUES
+            (1, "hello");
+        "#,
+    )
+    .execute(&mut *conn)
+    .await?
+    .last_insert_rowid();
+
+    assert!(id == 1);
+
+    let res = sqlx::query(
+        r#"
+          SELECT * FROM foo
+          WHERE contact_id=1
+        "#,
+    )
+    .fetch_one(&mut *conn)
+    .await?;
+
+    // get() requires trait Row
+    assert!(&res.get::<String, _>("first_name") == "hello");
 
     Ok(())
 }
