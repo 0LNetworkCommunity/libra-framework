03632200982b33d65e33136ce1517e115462ec41
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-10-10 14:18:06 -0400

    scaffold loading with sqlite

--- warehouse/src/migrate.rs
+++ warehouse/src/migrate.rs
@@ -1,9 +1,9 @@
-use sqlx::PgPool;
+use sqlx::SqlitePool;
 
-pub async fn maybe_init(pool: &PgPool) -> anyhow::Result<()> {
+pub async fn maybe_init(pool: &SqlitePool) -> anyhow::Result<()> {
     // run through all the migrations in ./sql/migrations.
     // note: the path is relative to project root
     // and the naming of the files follow a pattern, see migrations/README.md
-    sqlx::migrate!("tests/mock_migrations").run(pool).await?;
+    sqlx::migrate!("sql/migrations").run(pool).await?;
     Ok(())
 }
--- warehouse/tests/init.rs
+++ warehouse/tests/init.rs
@@ -1,8 +1,23 @@
-use sqlx::PgPool;
+use sqlx::SqlitePool;
 
 #[sqlx::test]
-async fn can_init(pool: PgPool) -> anyhow::Result<()> {
+async fn can_init(pool: SqlitePool) -> anyhow::Result<()> {
     libra_warehouse::migrate::maybe_init(&pool).await?;
+
+    let mut conn = pool.acquire().await?;
+
+    let id = sqlx::query(
+        r#"
+          INSERT INTO foo (contact_id, first_name)
+          VALUES
+            (1, "hello");
+        "#,
+    )
+    .execute(&mut *conn)
+    .await?
+    .last_insert_rowid();
+
+    assert!(id == 1);
 
     Ok(())
 }
