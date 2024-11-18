e283b487ac234d5c47e018358616b5b1c3caf692
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-11-18 16:55:39 -0500

    removes warehouse

--- warehouse/tests/mock_migrations/1_mock_init.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE
-  foo (
-    contact_id int,
-    first_name varchar(80) NOT NULL
-  );
--- warehouse/tests/support/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod age_testcontainer;
-#[allow(dead_code)]
-pub mod fixtures;
-pub mod pg_testcontainer;
-
-pub mod age_init;
-pub mod neo4j_testcontainer;
