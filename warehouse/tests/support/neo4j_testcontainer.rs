7e6b83269956837e8d7f1bcecd2f11c19e8a267f
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-11-18 11:41:19 -0500

    add note on neo4j data science

--- warehouse/tests/support/neo4j_testcontainer.rs
+++ warehouse/tests/support/neo4j_testcontainer.rs
@@ -5,27 +5,17 @@ use testcontainers::{clients::Cli, core::WaitFor, Container, GenericImage, Runna
 // copy pasta from: https://www.reddit.com/r/rust/comments/1294pfy/help_testcontainers_client_does_not_live_long/?rdt=54538
 static CLI: Lazy<Cli> = Lazy::new(Cli::default);
 
-// // Note: testcontainers drops the container once it's out of scope. So for each
-// // test we should pass it along, even if we don't reference it.
-// // Otherwise, the docker contain will stop before you run the test
-// pub async fn get_test_age_pool<'a>() -> anyhow::Result<(PgPool, Container<'a, GenericImage>)> {
-//     let container = start_age_container();
-//     // prepare connection string
-//     let connection_string = &format!(
-//         "postgres://{DB_USER}:{DUMMY_PASSWORD}@127.0.0.1:{}/{DB_NAME}",
-//         container.get_host_port_ipv4(5432)
-//     );
-
-//     let sqlx_pool = PgPool::connect(connection_string).await?;
-//     println!("database container started at: {}\n", &connection_string);
-
-//     Ok((sqlx_pool, container))
-// }
+// Note: testcontainers drops the container once it's out of scope. So for each
+// test we should pass it along, even if we don't reference it.
+// Otherwise, the docker contain will stop before you run the test
 
 pub fn start_neo4j_container<'a>() -> Container<'a, GenericImage> {
     let container = GenericImage::new("neo4j", "5.25.1-community")
         .with_env_var("NEO4J_AUTH".to_owned(), "none".to_owned())
+        // NOTE: To included data science modules in neo4j use:
+        // .with_env_var("NEO4J_PLUGINS".to_owned(), r#"["graph-data-science"]"#);
         .with_wait_for(WaitFor::message_on_stdout("Started."));
+
 
     let image = RunnableImage::from(container);
     // need to wrap the docker cli in a once_cell so that the borrow does not cause issues when container is passed along
