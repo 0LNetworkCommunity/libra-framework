6781a0e23cc3a40892c03dec7ba4d28e07c69500
Vale MacBeaver <vale_macbeaver@github.com>
2025-02-03 20:07:30 -0500

    build framework and lint

--- framework/cached-packages/src/lib.rs
+++ framework/cached-packages/src/lib.rs
@@ -1,2 +1,5 @@
+// NOTE: Diem generated code does not have
+// recent lint rules introduced in 2024
+#[allow(clippy::doc_lazy_continuation)]
 pub mod libra_framework_sdk_builder;
 pub mod libra_stdlib;
