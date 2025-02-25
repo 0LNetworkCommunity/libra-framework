64208828a3de6b3d9b81aac4f0aaefe624049718
Paoletta Fitz Polecat <paoletta_fitz_polecat@github.com>
2025-02-25 18:30:38 -0300

    [breaking] move semi-compatible upgrade test, and rename files.

--- upgrade-tests/tests/mainnet_breaking.rs
+++ upgrade-tests/tests/mainnet_breaking.rs
@@ -8,20 +8,6 @@ use libra_smoke_tests::libra_smoke::LibraSmoke;
 // do the same as above, but use the "arbitrary" upgrade policy to force an
 // upgrade.
 //
-/// Force upgrade Libra
-#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
-async fn force_upgrade_mainnet_libra() -> anyhow::Result<()> {
-    let mut s = LibraSmoke::new_with_target(Some(1), None, ReleaseTarget::Mainnet)
-        .await
-        .context("could not start libra smoke")?;
-    support::upgrade_multiple_impl(
-        &mut s,
-        "upgrade-single-lib-force",
-        vec!["1-libra-framework"],
-    )
-    .await?;
-    Ok(())
-}
 
 /// Upgrade all modules
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
