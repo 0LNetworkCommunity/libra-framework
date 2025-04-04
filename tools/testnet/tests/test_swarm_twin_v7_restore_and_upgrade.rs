2f9b444f0c6f2d9d322f63a2b3d3795cf3ab19c2
Crispin Cony <crispin_cony@github.com>
2025-04-03 20:18:44 -0400

    update twin test for replace validators and update framework

--- tools/testnet/tests/test_swarm_twin_v7_restore_and_upgrade.rs
+++ tools/testnet/tests/test_swarm_twin_v7_restore_and_upgrade.rs
@@ -1,4 +1,3 @@
-use libra_framework::release::ReleaseTarget;
 use libra_rescue::test_support::setup_v7_reference_twin_db;
 use libra_smoke_tests::libra_smoke::LibraSmoke;
 use libra_testnet::twin_swarm;
@@ -10,10 +9,12 @@ async fn test_twin_smoke_from_v7_rescue_and_upgrade() -> anyhow::Result<()> {
     let dir = setup_v7_reference_twin_db()?;
 
     let mut smoke = LibraSmoke::new(Some(2), None).await?;
-    let framework_mrb_path = ReleaseTarget::Head.find_bundle_path().ok();
-    twin_swarm::awake_frankenswarm(&mut smoke, Some(dir), framework_mrb_path).await?;
 
-    // checks if 0x1::founder module is present
+    let modified_head_mrb = libra_framework::upgrade_fixtures::fixtures_path()
+        .join("upgrade-multi-lib/head_all_your_base.mrb");
+    twin_swarm::awake_frankenswarm(&mut smoke, Some(dir), Some(modified_head_mrb)).await?;
+
+    // checks if 0x1::all_your_base module is present
     // should use api to list the modules installed on 0x1
     let client = smoke.client();
     let modules = client.get_account_modules("0x1".parse()?).await?;
@@ -24,10 +25,11 @@ async fn test_twin_smoke_from_v7_rescue_and_upgrade() -> anyhow::Result<()> {
             .try_parse_abi()
             .expect("failed to parse module");
         if let Some(m) = &new_m.abi {
-            return m.name.to_string().contains("libra_coin");
+            return m.name.to_string().contains("all_your_base");
         }
         false
     });
     println!("module exists: {}", exists);
+    assert!(exists, "module not found");
     Ok(())
 }
