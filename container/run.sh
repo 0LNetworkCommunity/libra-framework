67a8c5674bed1b1db26e5e4a447d9716450bee2b
David Boreham <david@bozemanpass.com>
2025-07-30 19:25:54 -0600

    Add simple config/run script for container

---
+++ container/run.sh
@@ -0,0 +1,17 @@
+# LIBRA_CONTAINER_MODE : validator|vfn|fullnode
+# Check if this container has already been configured
+libra_home=/root/.libra
+file_indicating_already_configured="fullnode.yml"
+if [[ ! -f ${libra_home}/${file_indicating_already_configured} ]]; then
+	echo "No existing config detected, initializing as a fullnode..."
+	# If not, run libra config
+	libra config fullnode-init
+	echo "Initialized"
+else
+	echo "Container already configured"
+fi
+# Otherwise fall through to start node
+# Start node
+echo "Starting libra node"
+libra node
+
