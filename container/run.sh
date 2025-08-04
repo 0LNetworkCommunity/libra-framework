41cd26acfb4d2e5a1d418c27d780bf9b34d6f135
David Boreham <david@bozemanpass.com>
2025-08-03 20:17:14 -0600

    Refactor container files (#433)
    Co-authored-by: David Boreham <david@bozemanpas.com>

---
+++ container/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# LIBRA_CONTAINER_MODE : validator|vfn|fullnode
+# Currently only supports fullnode mode
+
+# Hack to work around the libra tools not allowing arbitrary config paths
+export HOME=/mnt/libra
+
+# Check if this container has already been configured
+libra_home=${HOME}/.libra
+file_indicating_already_configured="fullnode.yml"
+if [[ ! -f ${libra_home}/${file_indicating_already_configured} ]]; then
+	echo "No existing config detected, initializing as a fullnode..."
+	# If not, run libra config
+	libra config fullnode-init --archive-mode false
+	result=$?
+	if [[ $result != 0 ]]; then
+		echo "Fatal Error: libra config failed"
+		exit 1
+	fi
+	echo "Initialized"
+else
+	echo "Container already configured"
+fi
+# Otherwise fall through to start node
+# Start node
+echo "Starting libra node"
+libra node
